cee215a4cb89f5a53bc30f17f6351a17f1ee85eb
Andrei <andreibvmw@gmail.com>
2021-12-06 00:21:31 +0200

    Added empty game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+/**
+ * Created Date: 12/6/2021
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	
+}
