12994e3c073de083149fe3facc15c76ddfb4f914
Andrei <andreibvmw@gmail.com>
2021-12-07 00:53:45 +0200

    Added logic to handle pushing player to ramp

--- Assets/Scripts/Player/PlayerStates/PlayerIdleState.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerIdleState.cs
@@ -5,21 +5,33 @@
  * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
  */
 
+using UnityEngine;
+
 namespace Player.PlayerStates {
 	public sealed class PlayerIdleState : BasePlayerState {
+		private bool _hasTouchedScreen;
+
+		/// <inheritdoc />
 		public override  void EnterState(PlayerManager player) {
-			throw new System.NotImplementedException();
+			player.Reset();
 		}
 
+		/// <inheritdoc />
 		public override void UpdateState(PlayerManager player) {
+			if (Input.GetMouseButtonDown(0) && !this._hasTouchedScreen) {
+				this._hasTouchedScreen = true;
+				player.PushPlayerToRamp();
+				player.SwitchState(player.OnRampState);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void OnTriggerEnter(Collider other, PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 
-		public override void OnTriggerEnter(PlayerManager player) {
-			throw new System.NotImplementedException();
-		}
-
-		public override void OnCollisionEnter(PlayerManager player) {
+		/// <inheritdoc />
+		public override void OnCollisionEnter(Collision other, PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 	}
