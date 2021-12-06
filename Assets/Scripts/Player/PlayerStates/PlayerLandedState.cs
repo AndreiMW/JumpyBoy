bcb640ba67eacf450401754f67abc0976da46fbb
Andrei <andreibvmw@gmail.com>
2021-12-07 00:54:42 +0200

    Added logic to handle player landing logic

--- Assets/Scripts/Player/PlayerStates/PlayerLandedState.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerLandedState.cs
@@ -5,21 +5,31 @@
 * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
 */
 
+using UnityEngine;
+
 namespace Player.PlayerStates {
 	public sealed class PlayerLandedState : BasePlayerState {
-		public override void EnterState(PlayerManager player) {
-			throw new System.NotImplementedException();
+		
+		/// <inheritdoc />
+		public override async void EnterState(PlayerManager player) {
+			player.ResetConstraints();
+			player.Slow();
 		}
 
+		/// <inheritdoc />
 		public override void UpdateState(PlayerManager player) {
+			if (player.Rigidbody.velocity.z == 0.0f) {
+				player.SwitchState(player.IdleState);
+			}
+		}
+
+		/// <inheritdoc />
+		public override void OnTriggerEnter(Collider other, PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 
-		public override void OnTriggerEnter(PlayerManager player) {
-			throw new System.NotImplementedException();
-		}
-
-		public override void OnCollisionEnter(PlayerManager player) {
+		/// <inheritdoc />
+		public override void OnCollisionEnter(Collision other, PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 	}
