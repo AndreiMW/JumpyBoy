81c28b0c799497e897440035229ce35833e2b56a
Andrei <andreibvmw@gmail.com>
2021-12-07 00:54:54 +0200

    Added collider and collision parameters

--- Assets/Scripts/Player/PlayerStates/BasePlayerState.cs
+++ Assets/Scripts/Player/PlayerStates/BasePlayerState.cs
@@ -5,11 +5,34 @@
  * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
  */
 
+using UnityEngine;
+
 namespace Player.PlayerStates {
 	public abstract class BasePlayerState {
+		/// <summary>
+		/// Method for state to handle state entering.
+		/// </summary>
+		/// <param name="player">The player controller.</param>
 		public abstract void EnterState(PlayerManager player);
+		
+		/// <summary>
+		/// Method for state to handle update loops.
+		/// </summary>
+		/// <param name="player">The player controller.</param>
 		public abstract void UpdateState(PlayerManager player);
-		public abstract void OnTriggerEnter(PlayerManager player);
-		public abstract void OnCollisionEnter(PlayerManager player);
+		
+		/// <summary>
+		/// Method for the state to handle trigger entering.
+		/// </summary>
+		/// <param name="other">The object trigger.</param>
+		/// <param name="player">The player controller.</param>
+		public abstract void OnTriggerEnter(Collider other,PlayerManager player);
+		
+		/// <summary>
+		/// Method for the state to handle collision entering.
+		/// </summary>
+		/// <param name="other">The object it collided with.</param>
+		/// <param name="player">The player controller.</param>
+		public abstract void OnCollisionEnter(Collision other,PlayerManager player);
 	}	
 }
