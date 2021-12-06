984e3de1c74001da08fa004192e6004fcbe0b787
Andrei <andreibvmw@gmail.com>
2021-12-07 00:54:16 +0200

    Added logic to handle collision with ramp end trigger collider

--- Assets/Scripts/Player/PlayerStates/PlayerOnRampState.cs
+++ Assets/Scripts/Player/PlayerStates/PlayerOnRampState.cs
@@ -5,21 +5,29 @@
 * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
 */
 
+using UnityEngine;
+
 namespace Player.PlayerStates { 
 	public sealed class PlayerOnRampState : BasePlayerState {
+		
+		/// <inheritdoc />
 		public override void EnterState(PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 
+		/// <inheritdoc />
 		public override void UpdateState(PlayerManager player) {
-			throw new System.NotImplementedException();
 		}
 
-		public override void OnTriggerEnter(PlayerManager player) {
-			throw new System.NotImplementedException();
+		/// <inheritdoc />
+		public override void OnTriggerEnter(Collider other, PlayerManager player) {
+			if (other.CompareTag("RampEnd")) {
+				player.SwitchState(player.InAirState);
+			}
 		}
 
-		public override void OnCollisionEnter(PlayerManager player) {
+		/// <inheritdoc />
+		public override void OnCollisionEnter(Collision other, PlayerManager player) {
 			throw new System.NotImplementedException();
 		}
 	}
