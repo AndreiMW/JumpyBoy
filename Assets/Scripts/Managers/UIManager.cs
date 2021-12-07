2542095a5f11d2a27bf074e42904700e3b5acb86
Andrei <andreibvmw@gmail.com>
2021-12-07 20:05:37 +0200

    Implemented singleton and added public method to set boost amount

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -7,8 +7,23 @@
 
 using UnityEngine;
 
+using UI.RocketBoost;
+
 namespace Managers {
 	public class UIManager : MonoBehaviour {
-	
+		private static UIManager s_instace;
+		public static UIManager Instance => s_instace ?? (s_instace = FindObjectOfType<UIManager>());
+
+		[SerializeField] 
+		private RocketBoostView _rocketBoostView;
+
+		/// <summary>
+		/// Set the fill amount for the boost left.
+		/// </summary>
+		/// <param name="boostAmount">The available boost amoount.</param>
+		public void SetBoostAmount(float boostAmount) {
+			this._rocketBoostView.SetBoostAmount(boostAmount);
+		}
+		
 	}
 }
