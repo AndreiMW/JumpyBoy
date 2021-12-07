9e311460abe18bda59fc268de8c0c85d306c7a02
Andrei <andreibvmw@gmail.com>
2021-12-07 19:56:04 +0200

    Added UIManager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,14 @@
+/**
+* Created Date: 12/7/2021
+* Author: Andrei-Florin Ciobanu
+* 
+* Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
+*/
+
+using UnityEngine;
+
+namespace Managers {
+	public class UIManager : MonoBehaviour {
+	
+	}
+}
