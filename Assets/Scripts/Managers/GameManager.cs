1379d14a54bac283f228f7c437302b614dfb158c
Andrei <andreibvmw@gmail.com>
2021-12-07 21:10:47 +0200

    Added logic to upgrade boost and ramp levels

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,8 +12,41 @@ namespace Managers {
 		private static GameManager s_instace;
 		public static GameManager Instance => s_instace ?? (s_instace = FindObjectOfType<GameManager>());
 
-		[SerializeField] private Transform _distanceStartPosition;
-
+		[SerializeField] 
+		private Transform _distanceStartPosition;
 		public Vector3 DistanceStartPosition => this._distanceStartPosition.position;
+
+		private int _rampBoostLevel = 1;
+		public int RampBoostLevel => this._rampBoostLevel;
+		
+		private int _boostDurationLevel = 1;
+		public int BoostDurationLevel => this._boostDurationLevel;
+		
+		#region Public
+
+		/// <summary>
+		/// Upgrade ramp level.
+		/// </summary>
+		public void UpgradeRampLevel() {
+			if (this._rampBoostLevel == 10) {
+				return;
+			}
+			this._rampBoostLevel++;
+			UIManager.Instance.SetRampLevel(this._rampBoostLevel);
+		}
+		
+		/// <summary>
+		/// Upgrade boost level.
+		/// </summary>
+		public void UpgradeBoostLevel() {
+			if (this._boostDurationLevel == 10) {
+				return;
+			}
+			this._boostDurationLevel++;
+			UIManager.Instance.SetBoostLevel(this._boostDurationLevel);
+		}
+		
+		#endregion
+		
 	}
 }
