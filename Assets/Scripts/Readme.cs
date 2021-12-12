5679b161803f2275313cf6f357ed8b69d34509cd
Andrei <andreibvmw@gmail.com>
2021-12-12 13:26:22 +0200

    Added Readme gameobject and script

---
+++ Assets/Scripts/Readme.cs
@@ -0,0 +1,13 @@
+/**
+ * Created Date: 12/12/2021
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2021 Andrei-Florin Ciobanu. All rights reserved. 
+ */
+
+using UnityEngine;
+
+public class Readme : MonoBehaviour {
+	[TextArea(1,200)]
+	[SerializeField] private string _text;
+}
